a1bf1bba6a76f58e4326e1ed95d157dd26c60aaf
Kian Ryan <kian@orangetentacle.co.uk>
2013-03-16 17:55:49 +0000

    Solution implementing identifying relationship

--- OrphanedObjects/Models/Child.cs
+++ OrphanedObjects/Models/Child.cs
@@ -1,9 +1,14 @@
-﻿namespace OrphanedObjects.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OrphanedObjects.Models
 {
     public class Child
     {
+        [Key, Column(Order = 1)]
         public virtual int Id { get; set; }
 
+        [Key, Column(Order = 2)]
         public virtual int ParentId { get; set; }
         public virtual Parent Parent { get; set; }
     }
